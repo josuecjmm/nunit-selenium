b3e22ebaf902e453d853dbd1e8b3cc425ae5f7cd
Connorturok27 <josuecjmm@gmail.com>
2021-02-17 11:24:16 -0600

    Set up of initial files

---
+++ Selenium/PageObjects/LoginPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+
+namespace Selenium.PageObjects
+{
+    class LoginPage : Page
+    {
+        private By emailInput = By.CssSelector("#email"); 
+        private By passwordInput = By.CssSelector("#password");
+        private By submitBtn = By.CssSelector("button[type='submit']");
+        private By alert = By.CssSelector(".alert strong");
+
+        public void fillLoginForm(string email, string password)
+        {
+            base.SendKeys(emailInput, email);
+            base.SendKeys(passwordInput, password);
+            base.Click(submitBtn);
+        }
+
+        public string getAlert()
+        {
+            return base.GetValue(alert);
+        }
+    }
+}
